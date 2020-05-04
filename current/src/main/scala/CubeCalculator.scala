3ea633b5b0e5aa40eb39580cdafc3032b4444c08
Thomas Deniffel <t.deniffel@canary-works.com>
2020-05-04 21:44:43 +0200

    gol implemenation done withou cli

--- current/src/main/scala/CubeCalculator.scala
+++ current/src/main/scala/CubeCalculator.scala
@@ -3,3 +3,135 @@ object CubeCalculator extends App {
     x * x * x
   }
 }
+
+object Cell {
+  def evolve(state: Boolean, neighbourCount: Int) = {
+    if (state && neighbourCount == 2 || neighbourCount == 3) {
+      true
+    } else {
+      false
+    }
+  } 
+}
+
+class InvalidBoard extends RuntimeException {
+  
+}
+
+object Board {
+  def create(rows: Int, columns: Int) = {
+    Array.ofDim[Boolean](rows, columns)
+  }
+
+  def rows(board: Array[Array[Boolean]]) = {
+    board.size
+  }
+
+  def cols(board: Array[Array[Boolean]]) = {
+    if (rows(board) == 0)
+      0
+    else
+      board(0).size
+  }
+
+  def evolve(board: Array[Array[Boolean]]) = {
+    val newBoard = clone(board)
+    val rowRange = 0 until board.size 
+    val columnRange = 0 until board(0).size
+
+    rowRange.foreach { r =>
+      columnRange.foreach { c => 
+        newBoard(r)(c) = Cell.evolve(newBoard(r)(c), countNeighbours(newBoard, r, c))
+      }
+    }
+    newBoard
+  }
+
+  def makeAlive(board: Array[Array[Boolean]], row: Int, column: Int) = {
+    val newBoard = clone(board)
+    newBoard(row)(column) = true;
+    newBoard
+  }
+
+  def isAlive(board: Array[Array[Boolean]], row: Int, column: Int) = {
+    board(row)(column)  
+  }
+
+  def countNeighbours(board: Array[Array[Boolean]], row: Int, column: Int) = {
+    val neighbours = getNeigbours(board, row, column) 
+    neighbours.foldLeft(0)((res, cell) => if(cell) res + 1 else res)
+    // val neighbours = getNeighbours(board: Array[Array[Boolean]], row, column) 
+  }
+
+  def getNeigbours(board: Array[Array[Boolean]], row: Int, column: Int) = {
+    var result: Array[Boolean] = Array() 
+    val rowRange = row - 1 until (row + 2)
+    val columnRange = column -1 until (column + 2)
+
+    rowRange.foreach { r =>
+      columnRange.foreach { c => 
+        if (isInBoard(board, r, c) && !(r == row && c == column)) {
+          result = result :+ board(r)(c)
+        }
+      }
+    }
+    result
+  }
+
+  def isInBoard(board: Array[Array[Boolean]], row: Int, column: Int) = {
+    row >= 0 && row < board.size && column >= 0 && column < board(0).size
+  }
+
+
+  def clone(board: Array[Array[Boolean]]) = {
+    board.map(row => row.map(column => column))
+  }
+
+  def parse(boardString: String) = {
+    var board: Array[Array[Boolean]] = Array()
+    var rows: Array[String] = Array()
+    val pattern = "[^\\.\\*\\n]".r
+    pattern.findFirstMatchIn(boardString) match {
+      case Some(_) => throw new InvalidBoard()
+      case None => if (boardString == "") {
+        board = create(0,0)
+      } else {
+        rows =  boardString.split('\n')
+        board = create(rows.size, rows(0).size)
+      }
+    }
+    initializeBoard(board, rows)
+    board
+  }
+
+  def toString(board: Array[Array[Boolean]]) = {
+    var result = ""
+    if (board.size == 0) {
+      result
+    } else {
+      (0 until board.size).foreach { r => 
+        (0 until board(0).size).foreach { c => 
+          result = result + (if(board(r)(c)) "*" else ".")
+        }
+        result = result + "\n"
+      }
+    }
+    result
+  }
+
+  def initializeBoard(board: Array[Array[Boolean]], rows: Array[String]) = {
+    (0 until board.size).foreach { r => 
+      (0 until board(0).size).foreach { c => 
+        board(r)(c) = if(rows(r)(c) == '*') true else false 
+      }
+    }
+  }
+
+  
+  def equal(board: Array[Array[Boolean]], other: Array[Array[Boolean]]) = {
+    true
+  }
+
+  // Map(0 -> "zero", 1 -> "one");
+  // 
+}
--- current/src/test/scala/CubeCalculatorTest.scala
+++ current/src/test/scala/CubeCalculatorTest.scala
@@ -1,7 +1,170 @@
 import org.scalatest.FunSuite;
 
+
 class CubeCalculatorTest extends org.scalatest.FunSuite {
   test("CubeCalculator.cube") {
     assert(CubeCalculator.cube(3) === 27)
+  }
+
+  test("dead cell, no neighbour, stays dead") {
+
+    assert(Cell.evolve(false, 0) === false)
+  }
+
+  test("alive cell, no neighbour, dies") {
+    assert(Cell.evolve(true, 0) === false)
+  }
+
+  test("alive cell, one neigbour, dies") {
+    assert(Cell.evolve(true, 1) === false)
+  }  
+
+  test("alive cell, two neigours, stays alive") {
+    assert(Cell.evolve(true, 2) === true)
+  }
+
+  test("dead cell, two neigbours, stays dead") {
+    assert(Cell.evolve(false, 2) === false)
+  }
+
+  test("Any live cell with more than three live neighbours dies, as if by overcrowding") {
+    assert(Cell.evolve(true, 4) === false)
+  }
+
+  test("Any live cell with two live neighbours lives on to the next generation.") {
+    assert(Cell.evolve(true, 2) === true)
+  }
+
+  test("Any live cell with three live neighbours lives on to the next generation.") {
+    assert(Cell.evolve(true, 3) === true)
+  }
+
+  test("Any dead cell with exactly three live neighbours becomes a live cell.") {
+    assert(Cell.evolve(false, 3) === true)
+  }
+}
+
+class NeigbourCountingTests extends org.scalatest.FunSuite {
+  
+  test("init") {
+    val board = Board.create(5, 5)
+    assert(board.size === 5)
+    assert(board(0).size === 5)
+  }
+
+  test("default board is all dead") {
+    val board = Board.create(5, 5)
+    board.foreach { row => 
+      row.foreach { cell => 
+        assert(cell == false)
+      }
+    }
+  }
+
+  test("one living cell in the center") {
+    var board = Board.create(5, 5)
+    board = Board.makeAlive(board, 2,2)
+    assert(Board.isAlive(board, 2,2) === true)
+  }
+
+  test("board is immuatable in makeAlive") {
+    var board = Board.create(5, 5)
+    Board.makeAlive(board, 2,2)
+    assert(Board.isAlive(board, 2,2) === false)
+  }
+
+  test("one live cell with no neigbours, couting") {
+    var board = Board.create(5, 5)
+    board = Board.makeAlive(board, 2,2)
+    assert(Board.countNeighbours(board, 2,2) == 0)
+  }
+
+  test("one neigbour") {
+    var board = Board.create(5, 5)
+    board = Board.makeAlive(board, 2,1)
+    board = Board.makeAlive(board, 2,2)
+    assert(Board.countNeighbours(board, 2,2) == 1)
+  }
+
+  test("all neighbours in center") {
+    var board = Board.create(5, 5)
+    board = Board.makeAlive(board, 2,2)
+    
+    board = Board.makeAlive(board, 1,1)
+    board = Board.makeAlive(board, 2,1)
+    board = Board.makeAlive(board, 1,2)
+    board = Board.makeAlive(board, 3,1)
+    board = Board.makeAlive(board, 1,3)
+    board = Board.makeAlive(board, 3,2)
+    board = Board.makeAlive(board, 2,3)
+    board = Board.makeAlive(board, 3,3) 
+
+    assert(Board.countNeighbours(board, 2,2) == (3*3-1))
+  }
+
+  test("asking for out of boundary") {
+    var board = Board.create(5, 5)
+    assert(Board.countNeighbours(board, -10, -20) === 0)
+  }
+}
+
+class MoveTests extends org.scalatest.FunSuite {
+  test("integration") {
+    var board = Board.create(5, 5)
+    board = Board.makeAlive(board, 2, 2)
+    board = Board.makeAlive(board, 1, 2)
+    board = Board.makeAlive(board, 3, 2)
+    board = Board.evolve(board)
+    assert(Board.isAlive(board, 2, 2) === false)
+    assert(Board.isAlive(board, 1, 2) === false)
+  }
+}
+
+class Parsing extends org.scalatest.FunSuite {
+
+  test("0 board") {
+    var board = Board.parse("")
+    assert(Board.rows(board) === 0)
+    assert(Board.cols(board) === 0)
+  }
+
+  test("all dead") {
+    var board = Board.parse("...\n...\n...\n")
+    assert(Board.rows(board) === 3)
+    assert(Board.cols(board) === 3)
+  }
+
+  test("bad input") {
+    assertThrows[InvalidBoard] { // Result type: Assertion
+      var board = Board.parse("abcdadsf\n.asdfasdf.\n.asdf\n")
+    }
+  }
+
+  test("one is alive") {
+    var board = Board.parse("...\n.*.\n..*\n")
+    assert(Board.isAlive(board, 1, 1) === true)
+    assert(Board.isAlive(board, 2, 2) === true)
+  }
+
+}
+
+class PrintingTest extends org.scalatest.FunSuite {
+  test("print empty") {
+    var board = Board.create(0,0)
+    assert(Board.toString(board) === "")
+  }
+
+  test("all dead") {
+    var board = Board.create(3,3)
+    assert(Board.toString(board) === "...\n...\n...\n")
+  }
+
+  test("some alive") {
+    var board = Board.create(3,3)
+    board = Board.makeAlive(board, 1, 1)
+    board = Board.makeAlive(board, 2, 2)
+    println("here") 
+    println(Board.toString(board))
+    assert(Board.toString(board) === "...\n.*.\n..*\n")
   }
 }
